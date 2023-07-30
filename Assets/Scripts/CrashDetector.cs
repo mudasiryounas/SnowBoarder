44291ba903a85fa6f40c3fce49d3c9c063ffd9bc
Mudasir Younas <mudasiryounas@gmail.com>
2023-07-30 19:48:36 +0300

    character finish line

---
+++ Assets/Scripts/CrashDetector.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrashDetector : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            Debug.Log("You lose!");
+        }
+    }
+
+}
