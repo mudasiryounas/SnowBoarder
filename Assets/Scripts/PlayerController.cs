8fb5924327cfb4ae0f1a28e2538869b1a79b289f
Mudasir Younas <mudasiryounas@gmail.com>
2023-08-03 16:53:29 +0300

    snow particles

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,13 +6,38 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb2d;
     [SerializeField] float torqueAmount = 10f;
+    [SerializeField] float normalSpeed = 20f;
+    [SerializeField] float boostSpeed = 30f;
+    SurfaceEffector2D surface;
+    [SerializeField] ParticleSystem snowParticles;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        surface = FindObjectOfType<SurfaceEffector2D>();
     }
 
     void Update()
+    {
+        RoratePlayer();
+        RespondToBoost();
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.tag == "Ground")
+        {
+            snowParticles.Play();
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other) {
+        if (other.gameObject.tag == "Ground")
+        {
+           snowParticles.Stop();
+        }
+    }
+
+    void RoratePlayer()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
@@ -22,6 +47,21 @@ public class PlayerController : MonoBehaviour
         {
             rb2d.AddTorque(-torqueAmount);
         }
+    }
+
+    void RespondToBoost()
+    {
+        // boost player speed when player is moving up on the incline
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            surface.speed = boostSpeed;
+        }
+        else
+        {
+            surface.speed = normalSpeed;
+        }
+
 
     }
+
 }
